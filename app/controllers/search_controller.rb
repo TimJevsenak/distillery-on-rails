c1e3ad7de4851f8c39c63ee307ee8d6b56b0255c
Tim Povse <55201493+xp3rt4g@users.noreply.github.com>
2021-04-13 12:40:10 +0200

    json now being parsed by rails

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   def index
       if params['location']
           @response = Openbrewerydb::Search.by_location(params['location'])
+          require 'json'
+          @parsed_json = JSON.parse(@response.body)
       end
   end
 end
