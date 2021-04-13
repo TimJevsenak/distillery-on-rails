4e8c27b8a53cc0a274cf57491e6c66dcddfb0481
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-04-13 16:47:53 +0200

    Added mailer to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'brewery-on-rails.herokuapp.com', protocol: 'https'}
+
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD'],
+    :domain => 'brewery-on-rails.herokuapp.com',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
