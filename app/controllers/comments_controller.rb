eb118114df5f7c895daf4b87f07b0eb222679429
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-04-20 19:35:08 +0200

    fixed reviews a bit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,18 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
 
+    respond_to do |format|
+      if @comment.text.nil? || @comment.rating.nil?
+        format.html { redirect_to brewery_path(id: @comment.brewery_id), alert: 'Rating or text can not be empty!' }
+    else
+
     if @comment.save
       redirect_to brewery_path(id: @comment.brewery_id)
     else
       render :new
     end
+  end
+  end
   end
 
   def update
