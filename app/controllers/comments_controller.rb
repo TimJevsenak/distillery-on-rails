c39b15c54caafc1a3e5cbc2aff9b91352aa59e35
xp3rt4g <machesys@gmail.com>
2021-04-20 22:23:24 +0200

    fixed ratings

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
 
-    respond_to do |format|
-      if @comment.text.nil? || @comment.rating.nil?
-        format.html { redirect_to brewery_path(id: @comment.brewery_id), alert: 'Rating or text can not be empty!' }
-    else
 
     if @comment.save
       redirect_to brewery_path(id: @comment.brewery_id)
     else
       render :new
     end
-  end
-  end
   end
 
   def update
@@ -24,6 +18,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:user_id, :brewery_id, :text, :rating)
+      params.require(:comment).permit(:user_id, :brewery_id, :text, :rating, :created_at, :updated_at)
     end
 end
