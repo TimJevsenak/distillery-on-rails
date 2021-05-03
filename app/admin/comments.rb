b1f25428c9f3e319ec27160a7e06fdc285ebfbe5
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-05-03 22:00:49 +0200

    When you forget a letter and try to fix it for a whole fucking hour!!!!!!!!!!!!!!!!!

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Comment, :as => "User Reviews" do
-  permit_params :user_id, :brewery_id, :text, :rating, :created_at, :updated_at, :blaclist
+  permit_params :user_id, :brewery_id, :text, :rating, :created_at, :updated_at, :blacklist
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
