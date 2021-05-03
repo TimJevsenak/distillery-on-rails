b4a73ce1080e65a672e92874fe16dee50486429e
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-05-03 21:00:25 +0200

    added blacklist to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_191204) do
+ActiveRecord::Schema.define(version: 2021_05_03_185712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_191204) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "brewery_id"
     t.integer "user_id"
+    t.boolean "blacklist", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
