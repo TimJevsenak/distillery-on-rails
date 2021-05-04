2aedafe0d306864a371e3f856d16f056b6515768
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-05-04 17:29:02 +0200

    comment ids were being duplicated. NO MORE

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,16 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
 
+    @comments = Comment.all
+    @highd = 0
+    @comments.each do |c|
+      if c.id > @highd
+        @highd = c.id
+      end
+    end
+    @highd += 1
+    @comment.id = @highd
+
 
     if @comment.save
       redirect_to brewery_path(id: @comment.brewery_id)
