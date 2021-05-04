a8cb93bfdf81b4fbccffe5138f71f82f1514105b
xp3rt4g <machesys@gmail.com>
2021-05-04 18:08:58 +0200

    added alerts when search is empty

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,10 @@ class SearchController < ApplicationController
           @response = Openbrewerydb::Search.by_location(params['location'])
           require 'json'
           @parsed_json = JSON.parse(@response.body)
+
+          if @parsed_json.length == 0
+            redirect_to root_path, alert: 'Search returned no results'
+          end
       end
   end
 
